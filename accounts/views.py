b369963d51b58108a9aebd836dcbd6ad6d223990
Lorenzo <cocco1805@gmail.com>
2025-07-08 15:08:34 +0200

    try logout with cookies

--- accounts/views.py
+++ accounts/views.py
@@ -49,6 +49,7 @@ def login_view(request):
 @api_view(['POST'])
 @permission_classes([permissions.IsAuthenticated])
 def logout_view(request):
+    request.user.auth_token = request.COOKIES.get('auth_token')
     request.user.auth_token.delete()
     logout(request)
     return Response({'message': 'Logged out successfully'})
