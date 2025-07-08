396ed60eb2aeea63b21ddc907ab693b89eaa0a36
Lorenzo <cocco1805@gmail.com>
2025-07-08 02:26:40 +0200

    cors

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -15,8 +15,8 @@ with open("secret.txt") as file:
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ecommerce-django-production-f55b.up.railway.app"]
-CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000", "http://localhost:8000", "https://ecommerce-django-production-f55b.up.railway.app"]
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = ["http://127.0.0.1:8000", "http://localhost:8000", "http://0.0.0.0:8000", "https://ecommerce-django-production-f55b.up.railway.app"]
+# CORS_ALLOW_ALL_ORIGINS = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 python manage.py makemigrations accounts products cart orders
-python manage.py makemigrations
 python manage.py migrate
 gunicorn ecommerce_api.wsgi:application --bind 0.0.0.0:8000
