741738a038fa144bcf7c93e1335635f676aec837
Lorenzo <cocco1805@gmail.com>
2025-07-09 19:39:40 +0200

    fix update_own_profile

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -47,4 +47,3 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'phone', 'address', 'date_of_birth', 'is_banned', 'created_at',
                   'updated_at']
         read_only_fields = ['id', 'created_at']
-        required
