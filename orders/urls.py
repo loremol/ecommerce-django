29ada1097a2cf1cd359fcb6e18d6a15b3c199113
Lorenzo <cocco1805@gmail.com>
2025-07-09 17:35:32 +0200

    added url for admin view to retrieve all the orders in the db

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from .views import get_own_orders, delete_order, get_order_details, checkout, update_order
+from .views import get_own_orders, delete_order, get_order_details, checkout, update_order, get_all_orders
 
 urlpatterns = [
+    path('all/', get_all_orders, name='all-orders'),  # Add this line
     path('', get_own_orders, name='order-list'),
+    path('<int:pk>/', get_order_details, name='order-details'),
     path('checkout/', checkout, name='order-list'),
     path('update/<int:pk>/', update_order, name='update-order'),
-    path('<int:pk>/', get_order_details, name='order-details'),
     path('cancel/<int:pk>/', delete_order, name='delete-order')
 ]
