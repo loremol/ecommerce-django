ddec11ec8efee873515290db80bed44b18e89da6
Lorenzo <cocco1805@gmail.com>
2025-07-09 02:49:07 +0200

    removed static things and all the admin.py

--- cart/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from cart.models import Cart, CartItem, Discount
-
-admin.site.register(Cart)
-admin.site.register(CartItem)
-admin.site.register(Discount)
--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -120,12 +120,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'productionfiles'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
