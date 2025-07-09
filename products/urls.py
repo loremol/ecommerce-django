d170f5095ec806dac372c5d4a946a67ba1a9e198
Lorenzo <cocco1805@gmail.com>
2025-07-09 15:20:43 +0200

    added update urls for products and categories

--- products/urls.py
+++ products/urls.py
@@ -5,9 +5,11 @@ from products import views
 urlpatterns = [
     path('categories/', views.CategoryListView.as_view(), name='category-list'),
     path('categories/create/', views.create_category, name='category-create'),
+    path('categories/update/<int:pk>/', views.update_category, name='update-category'),
     path('categories/delete/<int:pk>/', views.delete_category, name='delete-category'),
 
     path('products/', views.ProductListView.as_view(), name='product-list'),
     path('products/create/', views.create_product, name='product-create'),
-    path('products/<int:pk>/', views.delete_product, name='delete-product'),
+    path('products/update/<int:pk>/', views.update_product, name='update-product'),
+    path('products/delete/<int:pk>/', views.delete_product, name='delete-product'),
 ]
