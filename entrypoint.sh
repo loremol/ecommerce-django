f38a2ee9fa16aa003041dcb171279a59d2a1d1f8
Lorenzo <cocco1805@gmail.com>
2025-07-09 14:22:58 +0200

    removed collectstatic from entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,4 @@
 
 python manage.py makemigrations accounts products cart orders
 python manage.py migrate
-python manage.py collectstatic
 gunicorn ecommerce_api.wsgi:application --bind 0.0.0.0:8000
