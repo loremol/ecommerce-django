4b2f36d76517e097c4fcc6c18c15d52f20192b05
Lorenzo <cocco1805@gmail.com>
2025-07-09 19:39:10 +0200

    fix update_own_profile

--- accounts/views.py
+++ accounts/views.py
@@ -69,7 +69,7 @@ def update_own_profile(request):
     user = request.user
     serializer = UserSerializer(user, data=request.data)
     if serializer.is_valid():
-        serializer.save()
+        serializer.update(user, serializer.validated_data)
         return Response(serializer.data, status=status.HTTP_200_OK)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
