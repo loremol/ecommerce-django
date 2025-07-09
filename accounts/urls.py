10d2c1f203c3dd166cf44b229484d910be971443
Lorenzo <cocco1805@gmail.com>
2025-07-09 19:18:57 +0200

    added updateProfile URL

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+    path('update/', views.update_own_profile, name='update_profile'),
     path('ban/', views.ban_user, name='ban_user'),
     path('unban/', views.unban_user, name='unban_user'),
     path('users/', ListUsersView.as_view(), name='list_users'),
